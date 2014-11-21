d7b77cbe41cf3013bcf79cda4fedaee9aea4042e
akievet <akievet@gmail.com>
2014-11-21 17:16:01 -0500

    Adds standard deviation bar

--- app/assets/javascripts/thermometer.js
+++ app/assets/javascripts/thermometer.js
@@ -4,18 +4,29 @@ function thermometer(goalAmount, progressAmount, sdAmount, animate) {
 	var $thermo = $("#thermometer"),
 			$progress = $(".progress", $thermo),
 			$goal = $(".goal", $thermo),
-			percentageAmount;
+			$sd = $(".sd", $thermo),
+			percentageAmount,
+			sdPercentageAmount;
 
 	goalAmount = goalAmount || parseFloat($goal.text()),
 	progressAmount = progressAmount || parseFloat($progress.text()),
+	sdAmount = sdAmount || parseFloat($sd.text()),
 	percentageAmount = Math.min( Math.round(progressAmount / goalAmount * 1000)/10, 100);
+	sdPercentageAmount = Math.min( Math.round(sdAmount / goalAmount * 1000)/10, 100);
 	$goal.find(".amount").text(goalAmount);
 	$progress.find(".amount").text(progressAmount);
+	$sd.find(".amount").text(sdAmount);
 
 	$progress.find(".amount").hide();
+	$sd.find(".amount").hide();
 	if (animate !== false) {
 		$progress.animate({
 			"width": percentageAmount + "%"
+		}, 1200, function(){
+			$(this).find(".amount").fadeIn(500);
+		});
+		$sd.animate({
+			"width": sdPercentageAmount + "%"
 		}, 1200, function(){
 			$(this).find(".amount").fadeIn(500);
 		});
@@ -24,6 +35,10 @@ function thermometer(goalAmount, progressAmount, sdAmount, animate) {
 			"height": percentageAmount + "%"
 		});
 		$progress.find(".amount").fadeIn(500);
+		$sd.css({
+			"height": sdPercentageAmount + "%"
+		});
+		$sd.find(".amount").fadeIn(500);	
 	}
 }
 

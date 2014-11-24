8cb21178f319a0e9c885a8dbc21e257c13afe28a
akievet <akievet@gmail.com>
2014-11-24 14:46:07 -0500

    Thermometer words

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -9,5 +9,41 @@ function makeAPIRequest(){
 
 	function processApiData(data){
 		console.log(data);
+		var query = data.query;
+		var score = data.score;
+		var sd = data.sd;
+		var convo1 = data.convo1;
+		var convo2 = data.convo2;
+		var convo3 = data.convo3;
+		var words = data.words;
+		renderThermometer(score, sd);
+	};
+
+	function renderThermometer(score, sd){
+		var $section = $('.thermometer');
+		var $contentDiv = $("<div>", {id: "content"});
+		var $thermDiv = $("<div>", {id: "thermometer"});
+		var $trackDiv = $("<div>", {class: "track"});
+
+		var $goalDiv = $("<div>", {class: "goal"});
+		var $goalAmountDiv = $("<div>", {class: "amount"});
+		$goalDiv.append($goalAmountDiv);
+
+		var $progressDiv = $("<div>", {class: "progress"});
+		var $progressAmountDiv = $("<div>", {class: "amount"});
+		$progressDiv.append($progressAmountDiv);
+
+		var $sdDiv = $("<div>", {class: "sd"});
+		var $sdAmountDiv = $("<div>", {class: "amount"});
+		$sdDiv.append($sdAmountDiv);
+
+		$trackDiv.append($goalDiv);
+		$trackDiv.append($progressDiv);
+		$trackDiv.append($sdDiv);
+		$thermDiv.append($trackDiv);
+		$contentDiv.append($thermDiv);
+
+		$section.append($contentDiv);
+		thermometer(200, score, sd, true);
 	};
 }
--- app/assets/javascripts/thermometer.js
+++ app/assets/javascripts/thermometer.js
@@ -1,35 +1,25 @@
 function thermometer(goalAmount, progressAmount, sdAmount, animate) {
 	"use strict";
-
-	var $thermo = $("#thermometer"),
-			$progress = $(".progress", $thermo),
-			$goal = $(".goal", $thermo),
-			$sd = $(".sd", $thermo),
-			percentageAmount,
-			sdMarginLeft,
-			sdMarginRight,
-			sdPercentageAmount;
-
-	goalAmount = goalAmount || parseFloat($goal.find(".amount").data("amount"));
-	progressAmount = progressAmount || parseFloat($progress.find(".amount").data("amount"));
-	sdAmount = sdAmount || parseFloat($sd.find(".amount").data("amount"));
-
+	var $thermo = $("#thermometer");
+	var $progress = $(".progress");
+	var $goal = $(".goal");
+	var $sd = $(".sd");
+	var percentageAmount;
+	var sdMarginLeft;
+	var sdMarginRight;
+	var sdPercentageAmount;
+	
+	goalAmount = goalAmount;
+	progressAmount = progressAmount;
+	sdAmount = sdAmount;
 	percentageAmount = Math.min( Math.round(progressAmount / goalAmount * 1000)/10, 100);
-
 	sdPercentageAmount = Math.min( Math.round(sdAmount / goalAmount * 1000)/10, 100);
-
 	sdMarginLeft = percentageAmount - ( 0.5 * sdPercentageAmount );
 	sdMarginRight = ( 100 - percentageAmount ) - ( 0.5 * sdPercentageAmount );
-
-	$goal.find(".amount").data("amount");
-	$progress.find(".amount").data("amount");
-	$sd.find(".amount").data("amount");
-
 	$sd.css("margin", "0 " + sdMarginRight + "% 0 " + sdMarginLeft + "%");
-
-
 	$progress.find(".amount").hide();
 	$sd.find(".amount").hide();
+	
 
 	if (animate !== false) {
 		$progress.animate({

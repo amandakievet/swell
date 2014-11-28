ae43c8ad36eadeb50fc64059af24c0909b75149b
Aldric Giacomoni <trevoke@gmail.com>
2014-11-28 16:44:35 -0600

    Add comments to searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,29 @@
 class SearchesController < ApplicationController
-require 'csv'
+require 'csv' # This is a code smell.
+=begin
+The only logic inside a controller should be to handle incoming data, pass it
+off to an object that will do the real work, then return whatever data should
+be returned in whatever format it should be returned. There should be no CSV
+parsing, no JSON creating, nothing.
+=end
   def new
     example_array = ["obamacare", "christmas", "The Graduate", "bunnies", "bacon", "puppies"]
+    # Unless this is a placeholder for future logic, this is probably better
+    # off in the front-end
     @example = example_array.sample
   end
 
   def show
+    # Given that the HTML template for this action is empty,
+    # I'm guessing we don't need any of those to be instance variables
     @query = params[:query]
     searcher = ApiSearcher.new(@query)
     stats_hash = searcher.process_request
+
+    # All of this data formatting belongs in some method or object with the
+    # sole responsibility of doing this work
+    # See http://en.wikipedia.org/wiki/Single_responsibility_principle
+
     @score = searcher.manipulate_score(stats_hash[:mean])
     @sd = searcher.manipulate_sd(stats_hash[:sd])
     @words = stats_hash[:top_words]

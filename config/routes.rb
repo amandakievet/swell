7ca1bcd76e51f1db9ca8b229f7f8eefd89e7d9af
Aldric Giacomoni <trevoke@gmail.com>
2014-11-28 16:37:26 -0600

    Comment on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get 'searches/show'
-  root "searches#new"
+  root "searches#new" # It is standard to put the 'root' route at the top: it makes it clear where the starting point is.
 end
